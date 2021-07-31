979ae8031e38c91721637f9bdcd4daef282ac8fd
linox2000 <mllino1@hotmail.com>
2021-07-31 09:39:41 +0100

    finished the attack ,heal and reset functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,73 @@
 const ATTACK_VALUE = 10;
+const ATTACK_MONSTER_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHelth = chosenMaxLife;
 let currentPlayerHelth = chosenMaxLife;
-
+let hasBonusLife = true;
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
-const damage = dealMonsterDamage(ATTACK_VALUE)
-currentMonsterHelth-= damage;
+
+
+
+function reset(){
+    currentMonsterHelth = chosenMaxLife;
+    currentPlayerHelth = chosenMaxLife;
+    resetGame(chosenMaxLife)
+}
+function endRound(){
+    const initialPlayerHelth = currentMonsterHelth;
+    const playerDamage = dealPlayerDamage(ATTACK_MONSTER_VALUE);
+    currentPlayerHelth -= playerDamage;
+    if(currentPlayerHelth <=0 && hasBonusLife){
+        hasBonusLife =false;
+        removeBonusLife();
+        currentPlayerHelth = initialPlayerHelth;
+        setPlayerHealth(initialPlayerHelth);
+        alert('You would be dead but the bonus life saved you')
+    }
+    if (currentMonsterHelth <= 0 && currentPlayerHelth > 0) alert("You won");
+    else if (currentPlayerHelth <= 0 && currentMonsterHelth > 0)
+      alert("You lose");
+    else if (currentPlayerHelth <= 0 && currentMonsterHelth <= 0)
+      alert("You have draw!");
+      if(currentPlayerHelth <=0 || currentMonsterHelth <=0)
+      reset()
+
+}
+function attack(mode) {
+  let maxDamage;
+  if (mode === "ATTACK_VALUE") maxDamage = ATTACK_VALUE;
+  else if (mode === "STRONG_ATTACK") maxDamage = STRONG_ATTACK_VALUE;
+
+  const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHelth -= damage;
+    endRound();
 }
 
+function attackHandler() {
+    attack('ATTACK_VALUE')
+}
 
+function strongAttackHandler(){
+    attack("STRONG_ATTACK")
+}
 
+function healPlayerHendler(){
+    let healValue;
+    if(currentPlayerHelth >= chosenMaxLife - HEAL_VALUE){
+        alert("You can't heal to more than your initial value")
+        healValue = chosenMaxLife - currentPlayerHelth;
+    }else{
+        healValue = HEAL_VALUE;
+    }
 
-
-
-
-
-
-
-attackBtn.addEventListener('click',attackHandler)
+    increasePlayerHealth(healValue);
+    currentPlayerHelth +=healValue;
+    endRound()
+}
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHendler);
